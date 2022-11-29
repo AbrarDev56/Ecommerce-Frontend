9f83b0dc28be211e8c039768baca5220e58dc8e2
Dev <classifiedrealname@gmail.com>
2022-11-28 20:17:17 -0500

    dependencies update & setup

--- src/main.js
+++ src/main.js
@@ -3,12 +3,32 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import axios from 'axios'
 
-import './assets/main.css'
+axios.defaults.baseURL = 'http://localhost:1337/api/'
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
-app.use(router)
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
+app.use(router, axios)
 
 app.mount('#app')
+
+// import 'bootstrap/js/dist/alert';
+// import 'bootstrap/js/dist/button';
+// import 'bootstrap/js/dist/carousel';
+// import 'bootstrap/js/dist/collapse';
+// import 'bootstrap/js/dist/dropdown';
+// import 'bootstrap/js/dist/modal';
+// import 'bootstrap/js/dist/offcanvas';
+// import 'bootstrap/js/dist/popover';
+// import 'bootstrap/js/dist/scrollspy';
+// import 'bootstrap/js/dist/tab';
+// import 'bootstrap/js/dist/toast';
+// import 'bootstrap/js/dist/tooltip';
